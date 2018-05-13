bd71b9d4f7a4b944b6a357ece593dd576fd919e7
Wembo Mulumba <fabriceotepa@hotmail.com>
2018-05-13 16:31:22 -0400

    Add files via upload

---
+++ HotelGuestIO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelguest.io;
+
+/**
+ *
+ * @author personhubfab
+ */
+public class HotelGuestIO {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("The HotelGuest Application Started...");
+        
+           new LoginForm().setVisible(true);
+        
+    }
+    
+}
