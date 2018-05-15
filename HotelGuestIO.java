dfd27e0ec3e87f112f012c81c783d676669ace35
Wembo Mulumba <fabriceotepa@hotmail.com>
2018-05-15 09:37:02 -0400

    Update HotelGuestIO.java

--- HotelGuestIO.java
+++ HotelGuestIO.java
@@ -4,22 +4,53 @@
  * and open the template in the editor.
  */
 package hotelguest.io;
-
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 /**
  *
  * @author personhubfab
  */
 public class HotelGuestIO {
-    
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("The HotelGuest Application Started...");
+       
         
-           new LoginForm().setVisible(true);
+        System.out.println("-------- Checking Database Connection.... ------------");
+
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		System.out.println("Where is your MySQL JDBC Driver?");
+		e.printStackTrace();
+		return;
+	}
+
+	System.out.println("MySQL JDBC Driver Registered!");
+	Connection connection = null;
+
+	try {
+		connection = DriverManager
+		.getConnection("jdbc:mysql://localhost:3306/housing","root", "");
+
+	} catch (SQLException e) {
+		System.out.println("Connection Failed! Check output console");
+		e.printStackTrace();
+		return;
+	}
+
+	if (connection != null) {
+		System.out.println("You made it, take control your database now!");
+                             System.out.println("The HotelGuest Application Started...");
+                             new LoginForm().setVisible(true);
+	} else {
+		System.out.println("Failed to make connection!");
+	}
+        
+
         
     }
     
